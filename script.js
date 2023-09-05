c045e16f639cdf550ace983e02d06df8ce86ad6b
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-05 23:48:47 +0600

    Add API in JavaScript

---
+++ script.js
@@ -0,0 +1,4 @@
+const APILINK = "https://api.themoviedb.org/3/discover/movies?sort_by=popularity.desc&api_key=e8a36d998355fa53170ba22c4775d38d&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=e8a36d998355fa53170ba22c4775d38d&query=";
+
